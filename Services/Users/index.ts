0e02e7dbcdc1e0806b29ae12156d7384308b875f
EnricoBarbieri1997 <enrico.barbieri.1997@gmail.com>
2023-01-19 02:36:54 +0100

    feat: added login function without jwt

--- Services/Users/index.ts
+++ Services/Users/index.ts
@@ -7,6 +7,8 @@ import {randomBytes, pbkdf2 as callbackPbkdf2} from "crypto"
 import {promisify} from "util"
 import {SignUpData} from "./gRPC/Typescript/exchange/SignUpData"
 import {OperationOutcome} from "./gRPC/Typescript/exchange/OperationOutcome"
+import { LoginData } from "./gRPC/Typescript/exchange/LoginData"
+import { LoginResponse } from "./gRPC/Typescript/exchange/LoginResponse"
 
 const hashIterations = 10000
 const pbkdf2 = promisify(callbackPbkdf2)
@@ -33,12 +35,12 @@ postgresClient.connect().then(() =>
 		if(user?.iban === undefined) throw "Missing iban paramter"
 
 		const salt = randomBytes(128/8).toString('base64')
-		const saltedPassword = await pbkdf2(login.password, salt, hashIterations, 64, "sha512")
+		const saltedPassword = (await pbkdf2(login.password, salt, hashIterations, 64, "sha512")).toString('base64')
 	
 		const query = {
 			name: 'insert-user',
-			text: "INSERT INTO Users (email, password, name, iban, salt)" + 
-				" VALUES ('$1', '$2', '$3', '$4', $5)",
+			text: 'INSERT INTO "Users" (email, password, name, iban, salt)' + 
+				' VALUES ($1, $2, $3, $4, $5)',
 			values: [login.email, saltedPassword, user.name, user.iban, salt],
 		}
 		try
@@ -56,12 +58,40 @@ postgresClient.connect().then(() =>
 			success: true,
 		}
 	}
-	
-	type ServiceImplementation = {
-		exchange: grpc.handleUnaryCall<SignUpData, OperationOutcome>
+	async function loginFunction({ email, password }: LoginData): Promise<LoginResponse> {
+		if(email === undefined) throw "Missing email paramter"
+		if(password === undefined) throw "Missing password paramter"
+
+		const query = {
+			name: 'select-user',
+			text: 'SELECT * FROM "Users" WHERE email=$1',
+			values: [email],
+		}
+		try
+		{
+			const res = await postgresClient.query(query)
+			if(res.rowCount != 1) throw "No user found"
+			const user = res.rows[0]
+			const saltedPassword = (await pbkdf2(password, user.salt, hashIterations, 64, "sha512")).toString('base64')
+
+			if(user.password == saltedPassword) return {
+				token: "test",
+			}
+		}
+		catch(e)
+		{
+			throw "Invalid query " + e
+		}
+
+		throw "Generic error"
 	}
 	
-	const exchange: grpc.handleUnaryCall<SignUpData, OperationOutcome> = async (call, callback) => {
+	type ServiceImplementation = {
+		SignUp: grpc.handleUnaryCall<SignUpData, OperationOutcome>
+		Login: grpc.handleUnaryCall<LoginData, LoginResponse>
+	}
+	
+	const SignUp: grpc.handleUnaryCall<SignUpData, OperationOutcome> = async (call, callback) => {
 		try
 		{
 			const signUpResult = await signUpFunction(call.request)
@@ -74,9 +104,23 @@ postgresClient.connect().then(() =>
 			})
 		}
 	}
+	const Login: grpc.handleUnaryCall<LoginData, LoginResponse> = async (call, callback) => {
+		try
+		{
+			const signUpResult = await loginFunction(call.request)
+			callback(null, signUpResult)
+		}
+		catch(error)
+		{
+			callback({
+				code: grpc.status.FAILED_PRECONDITION,
+			})
+		}
+	}
 	
 	const implementation: ServiceImplementation = {
-		exchange
+		SignUp,
+		Login,
 	}
 	
 	server.addService(usersService.Users.service, implementation)
