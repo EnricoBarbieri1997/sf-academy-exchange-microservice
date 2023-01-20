e071fc6f0087d286fbf1bea4e27049cab57e0b21
Enrico Barbieri <enrico.barbieri.1997@gmail.com>
2023-01-20 09:21:32 +0100

    feat: :sparkles: added openapi specification and openapi-express initialization

---
+++ ApiGateway/index.ts
@@ -0,0 +1,78 @@
+import {env} from "process"
+import express from "express"
+import { readFileSync } from "fs"
+import {initialize} from "express-openapi"
+import { resolve as resolvePath, join as joinPath} from "path"
+import cors from "cors"
+import jwt, { Jwt } from "jsonwebtoken"
+import * as grpc from '@grpc/grpc-js'
+import * as protoLoader from '@grpc/proto-loader'
+
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use(function(req, res, next)
+{
+	if(env.PROTO_PATH === undefined) throw "Proto path missing"
+	const usersPackageDefinition = protoLoader.loadSync(joinPath(__dirname, env.PROTO_PATH, "users.proto"))
+	const usersProtoDescriptor = grpc.loadPackageDefinition(usersPackageDefinition)
+	const usersService = usersProtoDescriptor.exchange as any
+	res.locals.usersClient = new usersService.Users('users:9001', grpc.credentials.createInsecure())
+	next()
+})
+
+app.use(function(req, res, next)
+{
+	const headers = req.headers as any
+	if(headers !== undefined)
+	{
+		try
+		{
+			const authHeader = headers.authorization
+			const token = authHeader && authHeader.split(' ')[1]
+			res.locals.jwtPayload = jwt.verify(token, env.JWT_SECRET as string)
+		}
+		catch(e)
+		{
+			// console.log(e)
+		}
+	}
+	next()
+})
+
+initialize({
+	apiDoc: readFileSync(resolvePath(__dirname, 'src', 'api-doc.yml'), 'utf8'),
+	app,
+	securityHandlers: {
+		Token: function(req, scopes, definition)
+		{
+			const headers = req.headers as any
+			if(req.headers === undefined) return false
+			const authHeader = headers.authorization
+			const token = authHeader && authHeader.split(' ')[1]
+
+			if (token == null) return false
+
+			try
+			{
+				const payload = jwt.verify(token, env.JWT_SECRET as string) as jwt.JwtPayload
+				if(payload.user === undefined) return false
+			}
+			catch(e)
+			{
+				return false
+			}
+			return true
+		}
+	},
+})
+
+app.use(function (err, req, res, next)
+{
+	res.status(err.status).json(err)
+} as express.ErrorRequestHandler)
+
+app.listen(process.env.PORT ?? 8080)
