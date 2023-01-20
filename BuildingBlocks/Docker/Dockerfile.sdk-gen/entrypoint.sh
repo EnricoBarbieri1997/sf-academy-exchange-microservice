fc2220f5504215480327d5a967441172049093ad
Enrico Barbieri <enrico.barbieri.1997@gmail.com>
2023-01-20 15:47:17 +0100

    build: :construction_worker: added custom container for sdk build

---
+++ BuildingBlocks/Docker/Dockerfile.sdk-gen/entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+docker-entrypoint.sh "$@"
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -o)
+      outputfolder="$2"
+      shift # past argument
+      shift # past value
+      ;;
+		*)
+			shift
+			;;
+  esac
+done
+
+$current="${PWD}"
+
+mkdir "$outputfolder/src"
+cd $outputfolder
+# find ./ -maxdepth 1 -mindepth 1 -exec mv -t "$outputfolder/src" {} +
+mv /tmp/package.json ./package.json
+mv /tmp/tsconfig.json ./tsconfig.json
+
+npm install typescript
+npm install axios@1.2.2
+npx tsc --build
