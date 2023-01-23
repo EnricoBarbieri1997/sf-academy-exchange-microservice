6fae529f9ea2fa65bb1dc52d345ae5c6b412cb3d
EnricoBarbieri1997 <enrico.barbieri.1997@gmail.com>
2023-01-23 13:41:38 +0100

    feat: added cookie helpers

---
+++ Web/helpers/cookies.ts
@@ -0,0 +1,15 @@
+import { NextPageContext } from "next"
+
+export function getCookie(cookies: string, name: string)
+{
+	const keyIndex = cookies.indexOf(name)
+	if(keyIndex < 0) return ""
+	const valueStart = cookies.indexOf("=", keyIndex) + 1
+	const valueEnd = cookies.indexOf(";", valueStart)
+	return cookies.substring(valueStart, valueEnd >= 0 ? valueEnd : cookies.length)
+}
+
+export function getCookieFromContext(context: NextPageContext, name: string)
+{
+	return context.req && context.req.headers.cookie ? getCookie(context.req.headers.cookie, name) : ""
+}
