ad844f4f8a4bfabd5c40403a7fb3d450d89965b2
Enrico Barbieri <enrico.barbieri.1997@gmail.com>
2023-01-23 16:23:20 +0100

    feat: added withdraw endpoint

---
+++ Web/pages/api/deposit.ts
@@ -0,0 +1,24 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import {DefaultApi} from "sf-academy-exchanger-sdk"
+import { axiosAuthConfigs } from '../../helpers/authorization'
+import { getCookie, getCookieFromReq } from '../../helpers/cookies'
+
+type Data = boolean
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+)
+{
+	const data = req.body
+	data.value = parseFloat(data.value)
+	const api = new DefaultApi()
+	try
+	{
+		await api.deposit(data, axiosAuthConfigs(req))
+	}
+	finally
+	{
+		res.redirect("/transactions")
+	}
+}
